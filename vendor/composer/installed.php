627a6366760b57d173c35a59526947cd95abe704
alnazeerabdo <alnzyrbdalmnm90@gmail.com>
2025-07-17 21:58:48 +0200

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mynewvk/tier-pricing-table',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8de3f1fc9d62e18eb91ef8b33998ddbc8dfaa88d',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'mynewvk/tier-pricing-table' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8de3f1fc9d62e18eb91ef8b33998ddbc8dfaa88d',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
